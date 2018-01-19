bfdbce0ecbc00519f5b5f666be7548d30b2551e7
Luke Andrews <lukejandrews@hotmail.co.uk>
2018-01-19 21:22:18 +0000

    updated IF system to increment address on read or write

--- expansionIF.vhd
+++ expansionIF.vhd
@@ -33,6 +33,7 @@ architecture func of expansionIF is
 
 	signal bufferIn, bufferOut	: std_logic_vector(7 downto 0) := (others => '0');
 	signal outClockEn	:	std_logic := '0';
+	signal Addr_plus_one, AddressR : std_logic_vector(15 downto 0);
 
 begin
 
@@ -40,7 +41,8 @@ begin
 	clkO <= (not Clock) and outClockEn;
 	
 	-- setup the address register combinationally
-	addressStorage: entity work.DFF_rising generic map (16) port map ((not addressData) and Clock and enable, AddressIn, AddressOut);
+	addressStorage: entity work.DFF_rising generic map (16) port map (not((not addressData) and Clock and enable), AddressR, AddressOut);
+	Addr_plus_one <= std_logic_vector(unsigned(AddressOut) + 1);
 	
 	-- connect buffer lines to memory bus
 	streamOut <= bufferIn;
@@ -61,14 +63,21 @@ begin
 		if rising_edge(Clock) then
 		-- this is the clock edge where we control data
 		if outClockEn = '1' then outClockEn <= '0'; end if;
-		
+		-- reset the address input
+		AddressR <= (others => '0');
 		if addressData = '1' then
 			-- its data
+			-- connect add one to address
+			AddressR <= Addr_plus_one;
+			
 			if ReadWrite = '0' then 
 				-- its write
 				-- enable the clock
 				outClockEn <= '1';
 			end if;
+		else 
+			-- its address so conect in the address line
+			AddressR <= AddressIn;
 		end if;
 		end if;
 				
