c33cdb3253f6cf8afe806567da6c74f4b05d19c9
Luke Andrews <lukejandrews@hotmail.co.uk>
2018-02-20 16:12:21 +0000

    Focus Change - Working AFAIK
    This design works, and the project focus is now shifting to working on the modularity of the project

--- db/ip/sldaa04331a/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ db/ip/sldaa04331a/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : de719aaa34f9dbbd6d0a
+//   DESIGN_HASH   : a4e338d4e721e65a21c2
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'hde719aaa34f9dbbd6d0a };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'ha4e338d4e721e65a21c2 };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
